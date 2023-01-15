052e2c0bda1a969b33bb3ad2170d87be8b4c3df4
David Lin <lindavid1998@gmail.com>
2023-01-15 13:21:09 -0800

    Add minor styling updates

--- script.js
+++ script.js
@@ -51,9 +51,7 @@ async function getForecast(city) {
 
 async function loadPopularCities() {
     // appends weather summaries to sub header
-
     let cities = ['San Diego', 'London', 'Taipei', 'New York'];
-
     let output = [];
     for (let city of cities) {
         output.push(await createCard(city));
@@ -77,14 +75,13 @@ async function createCard(city) {
     // creates a div that summarizes the weather of input city
 
     try {
-        // fetch data
         let response = await fetch(
             `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}`,
             { mode: 'cors' }
         )
 
         let data = await response.json();
-        let temp = convertToKelvin(data.main.temp);
+        let temp = convertKelvin(data.main.temp);
         let weather = data.weather[0];
         let time = new Date((data.dt + data.timezone) * 1000);
 
@@ -108,20 +105,18 @@ function appendSummary(card, weatherID, temperature, time) {
     span.classList.add('temperature');
     span.innerHTML = temperature;
     summary.append(img, span);
-    
-    card.append(summary)
+    card.append(summary);
 }
 
 function appendDescription(card, city, weather) {
     let desc = createDiv('weather-description');
     let name = createDiv('name');
-    let conditions = createDiv('conditions')
+    let conditions = createDiv('conditions');
 
     name.textContent = city;
     conditions.textContent = weather;
     desc.append(name, conditions);
-    
-    card.append(desc)
+    card.append(desc);
 }
 
 function createDiv(className = '') {
@@ -130,12 +125,53 @@ function createDiv(className = '') {
     return div
 }
 
-function convertToKelvin(deg) {
+function convertKelvin(deg) {
     if (useMetric) {
-        return `${Math.round(deg - 273)} &#8451`;
+        return `${Math.round(deg - 273)}&#8451`; // C
     } else {
-        return `${Math.round((deg - 273.15) * 9/5 + 32)} &#8457`
+        return `${Math.round((deg - 273.15) * 9/5 + 32)}&#8457` // F
     }
+}
+
+function convertWindDegToDirection(deg) {
+    // input: deg -> Number
+    // output: string
+
+    switch (true) {
+        case (deg >= 30 && deg < 60):
+            return 'NE'
+        case (deg >= 60 && deg < 120):
+            return 'E'
+        case (deg >= 120 && deg < 150):
+            return 'SE'
+        case (deg >= 150 && deg < 210):
+            return 'S'
+        case (deg >= 210 && deg < 240):
+            return 'SW'
+        case (deg >= 240 && deg < 300):
+            return 'W'
+        case (deg >= 300 && deg < 330):
+            return 'NW'
+        default:
+            return 'N'
+    }
+}
+
+function convertDateToTime(time) {
+    // time -> Date object
+    // returns time of Date object in HH:MM AM/PM format
+
+    let hours = time.getUTCHours();
+    let minutes = '0' + time.getUTCMinutes();
+    minutes = minutes.slice(-2);
+
+    if (hours > 12) {
+        hours = hours - 12;
+        return `${hours}:${minutes} PM`
+    } else {
+        return `${hours}:${minutes} AM`
+    }
+    
 }
 
 function loadWeatherIcon(img, id, time) {
@@ -183,31 +219,27 @@ function loadWeatherIcon(img, id, time) {
             img.alt = 'Icon for cloudy conditions';
             break;
         default:
-            img.src = `./icons/question-mark.png`
-            img.alt = 'Missing icon';
+            img.src = `./icons/other.svg`
+            img.alt = 'Icon for atmospheric condition';
     }
 }
 
 async function loadSearchResult(city) {
     try {
-        // load weather
+        // fetch weather and forecast
         let weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}`, {mode: 'cors'})
         let weatherData = await weatherResponse.json();
-
-        // load forecast
         let forecastResponse = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${API_KEY}`, {mode: 'cors'})
         let forecastData = await forecastResponse.json();
+
         let timezone = forecastData.city.timezone;
-
         let forecast = groupForecastByDay(forecastData, timezone);
-        let highLows = getHighLow(forecast);
+        let highLows = getHighLowByDay(forecast);
 
-        // populate city name
+        // populate city name and country
         const cityName = document.querySelector('.location .city-name');
-        cityName.textContent = weatherData.name;
-
-        // populate country
         const country = document.querySelector('.location .country');
+        cityName.textContent = weatherData.name;
         country.textContent = weatherData.sys.country;
 
         // toggle search result view
@@ -249,7 +281,7 @@ function loadSectionOne(data) {
 
     // load temperature
     const temp = section.querySelector('.temperature');
-    temp.innerHTML = convertToKelvin(data.main.temp);
+    temp.innerHTML = convertKelvin(data.main.temp);
 
     // load weather
     const weatherDiv = section.querySelector('.weather');
@@ -257,17 +289,15 @@ function loadSectionOne(data) {
 
     // load feels like
     const feelsLike = section.querySelector('.feels-like');
-    feelsLike.innerHTML = `Feels Like: ${convertToKelvin(data.main.feels_like)}`
+    feelsLike.innerHTML = `Feels Like: ${convertKelvin(data.main.feels_like)}`
 }
 
 function loadSectionTwo(highLows) {
-    let currentDate = Object.keys(highLows)[0]
     const section = document.querySelector('.section-two');
-
+    const currentDate = Object.keys(highLows)[0];
     const high = section.querySelector('.high .value');
-    high.innerHTML = highLows[currentDate].high
-
     const low = section.querySelector('.low .value');
+    high.innerHTML = highLows[currentDate].high
     low.innerHTML = highLows[currentDate].low
 }
 
@@ -296,23 +326,6 @@ function loadSectionThree(data) {
     const sunsetDiv =  document.querySelector('#sunset .value');
     let sunset = new Date((data.sys.sunset + data.timezone) * 1000);
     sunsetDiv.textContent = convertDateToTime(sunset);
-}
-
-function convertDateToTime(time) {
-    // time -> Date object
-    // returns time of Date object in HH:MM AM/PM format
-
-    let hours = time.getUTCHours();
-    let minutes = '0' + time.getUTCMinutes();
-    minutes = minutes.slice(-2);
-
-    if (hours > 12) {
-        hours = hours - 12;
-        return `${hours}:${minutes} PM`
-    } else {
-        return `${hours}:${minutes} AM`
-    }
-    
 }
 
 function load4DayForecast(forecast, timezone, highLows) {
@@ -416,18 +429,15 @@ function loadDayForecast(div, forecast, timezone, highLow) {
 
     // populate high low 
     const high = div.querySelector('.high')
-    high.innerHTML = `<span>L: </span>${highLow.high}`
+    high.innerHTML = `<span>H: </span>${highLow.high}`
     const low = div.querySelector('.low')
     low.innerHTML = `<span>L: </span>${highLow.low}`
 
 }
 
-
-function getHighLow(forecast) {
+function getHighLowByDay(forecast) {
     // given object of forecast divided by day (keys), return object with high and lows (values) of each day (key)
-    // initialize output object
     let output = {};
-
     for (const day in forecast) {
         let dayForecast = forecast[day] // array of objects
         
@@ -435,34 +445,10 @@ function getHighLow(forecast) {
         let lows = dayForecast.map(timepoint => timepoint.main.temp_min)
 
         output[day] = {
-            'high': convertToKelvin(Math.max(...highs)),
-            'low': convertToKelvin(Math.min(...lows))
+            'high': convertKelvin(Math.max(...highs)),
+            'low': convertKelvin(Math.min(...lows))
         }
     }
 
     return output
-}
-
-function convertWindDegToDirection(deg) {
-    // input: deg -> Number
-    // output: string
-
-    switch (true) {
-        case (deg >= 30 && deg < 60):
-            return 'NE'
-        case (deg >= 60 && deg < 120):
-            return 'E'
-        case (deg >= 120 && deg < 150):
-            return 'SE'
-        case (deg >= 150 && deg < 210):
-            return 'S'
-        case (deg >= 210 && deg < 240):
-            return 'SW'
-        case (deg >= 240 && deg < 300):
-            return 'W'
-        case (deg >= 300 && deg < 330):
-            return 'NW'
-        default:
-            return 'N'
-    }
 }
